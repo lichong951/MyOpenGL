e93960b5f4a5687fd9465a82e0c220a990a03a69
lichong <lichong@7moor.com>
2017-07-30 23:43:41 +0800

    【Blog实例程序】三角形；【Book实例】3.8点

---
+++ MyOpenGLCoco/MyOpenGLCoco/Chapter3_8_Primitives.cpp
@@ -0,0 +1,208 @@
+//
+//  Chapter3_8_Primitives.cpp
+//  MyOpenGLCoco
+//
+//  Created by lichong on 2017/7/30.
+//  Copyright © 2017年 lichong. All rights reserved.
+//
+
+// Main entry point for GLUT based programs
+
+#include <GLTools.h>
+#include "GLMatrixStack.h"
+#include "GLFrame.h"
+#include "GLFrustum.h"
+#include "GLBatch.h"
+#include "GLGeometryTransform.h"
+
+#include <math.h>
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+/////////////////////////////////////////////////////////////////////////////////
+// An assortment of needed classes
+GLShaderManager		shaderManager;
+GLMatrixStack		modelViewMatrix;
+GLMatrixStack		projectionMatrix;
+GLFrame				cameraFrame;
+GLFrame             objectFrame;
+GLFrustum			viewFrustum;
+
+GLBatch				pointBatch;
+GLBatch				lineBatch;
+GLBatch				lineStripBatch;
+GLBatch				lineLoopBatch;
+GLBatch				triangleBatch;
+GLBatch             triangleStripBatch;
+GLBatch             triangleFanBatch;
+GLGeometryTransform	transformPipeline;
+M3DMatrix44f		shadowMatrix;
+GLfloat vGreen[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+GLfloat vBlack[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+// Keep track of effects step
+int nStep = 0;
+///////////////////////////////////////////////////////////////////////////////
+// This function does any needed initialization on the rendering context.
+// This is the first opportunity to do any OpenGL related tasks.
+void SetupRC(){
+    //为色彩缓冲区指定用于清除的值
+    glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
+    //初始化着色管理器
+    shaderManager.InitializeStockShaders();
+    //绘制深度检测
+    glEnable(GL_DEPTH_TEST);
+    //
+    transformPipeline.SetMatrixStacks(modelViewMatrix, projectionMatrix);
+    cameraFrame.MoveForward(-15.0f);
+    
+    // Some points, more or less in the shape of Florida
+    GLfloat vCoast[24][3] = {{2.80, 1.20, 0.0 }, {2.0,  1.20, 0.0 },
+        {2.0,  1.08, 0.0 },  {2.0,  1.08, 0.0 },
+        {0.0,  0.80, 0.0 },  {-.32, 0.40, 0.0 },
+        {-.48, 0.2, 0.0 },   {-.40, 0.0, 0.0 },
+        {-.60, -.40, 0.0 },  {-.80, -.80, 0.0 },
+        {-.80, -1.4, 0.0 },  {-.40, -1.60, 0.0 },
+        {0.0, -1.20, 0.0 },  { .2, -.80, 0.0 },
+        {.48, -.40, 0.0 },   {.52, -.20, 0.0 },
+        {.48,  .20, 0.0 },   {.80,  .40, 0.0 },
+        {1.20, .80, 0.0 },   {1.60, .60, 0.0 },
+        {2.0, .60, 0.0 },    {2.2, .80, 0.0 },
+        {2.40, 1.0, 0.0 },   {2.80, 1.0, 0.0 }};
+    
+    //批次处理
+    pointBatch.Begin(GL_POINTS, 24);
+    pointBatch.CopyVertexData3f(vCoast);
+    pointBatch.End();
+    
+}
+/////////////////////////////////////////////////////////////////////////
+void DrawWireFramedBatch(GLBatch* pBatch)
+{
+    //传递到存储着色器
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vGreen);
+    pBatch->Draw();
+    
+    // 设置后深度偏移量
+    glPolygonOffset(-1.0f, -1.0f);      // Shift depth values
+    glEnable(GL_POLYGON_OFFSET_LINE);// glDisable(GL_POLYGON_OFFSET_FILL);//开启面的深度偏移
+    
+    // Draw lines antialiased
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    // Draw black wireframe version of geometry
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glLineWidth(2.5f);
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vBlack);
+    pBatch->Draw();
+    
+    // Put everything back the way we found it
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glDisable(GL_POLYGON_OFFSET_LINE);
+    glLineWidth(1.0f);
+    glDisable(GL_BLEND);
+    glDisable(GL_LINE_SMOOTH);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Called to draw scene
+void RenderScene(void)
+{
+    //清除一个或一组特定的缓冲区
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    
+    modelViewMatrix.PushMatrix();
+    M3DMatrix44f mCamera;
+    cameraFrame.GetCameraMatrix(mCamera);
+    modelViewMatrix.MultMatrix(mCamera);
+    
+    M3DMatrix44f mObjectFrame;
+    objectFrame.GetMatrix(mObjectFrame);
+    modelViewMatrix.MultMatrix(mObjectFrame);
+    //传递到存储着色器
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vBlack);
+    
+    switch(nStep) {
+        case 0:
+            glPointSize(4.0f);
+            pointBatch.Draw();
+            glPointSize(1.0f);
+            break;
+        case 1:
+            glLineWidth(2.0f);
+            lineBatch.Draw();
+            glLineWidth(1.0f);
+            break;
+        case 2:
+            glLineWidth(2.0f);
+            lineStripBatch.Draw();
+            glLineWidth(1.0f);
+            break;
+        case 3:
+            glLineWidth(2.0f);
+            lineLoopBatch.Draw();
+            glLineWidth(1.0f);
+            break;
+        case 4:
+            DrawWireFramedBatch(&triangleBatch);
+            break;
+        case 5:
+            DrawWireFramedBatch(&triangleStripBatch);
+            break;
+        case 6:
+            DrawWireFramedBatch(&triangleFanBatch);
+            break;
+    }
+    
+    modelViewMatrix.PopMatrix();
+    
+    //将在后台缓冲区进行渲染，然后在结束时交换到前台
+    glutSwapBuffers();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Window has changed size, or has just been created. In either case, we need
+// to use the window dimensions to set the viewport and the projection matrix.
+//回调函数 窗口改变
+void ChangeSize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    viewFrustum.SetPerspective(35.0f, float(w) / float(h), 1.0f, 500.0f);
+    projectionMatrix.LoadMatrix(viewFrustum.GetProjectionMatrix());
+    modelViewMatrix.LoadIdentity();
+}
+int main(int argc, char* argv[])
+{
+    //设置当前工作目录，针对MAC OS X
+    gltSetWorkingDirectory(argv[0]);
+    //初始化GLUT库
+    glutInit(&argc, argv);
+    /*初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指
+     双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区*/
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+    //GLUT窗口大小，标题窗口
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("GL_POINTS");
+    //注册回调函数 窗口改变
+    glutReshapeFunc(ChangeSize);
+//    glutKeyboardFunc(KeyPressFunc);
+//    glutSpecialFunc(SpecialKeys);
+    //注册回调函数 渲染
+    glutDisplayFunc(RenderScene);
+    //渲染环境检测
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+//    
+//    
+    SetupRC();
+//消息循环
+    glutMainLoop();
+    return 0;
+}
---
+++ MyOpenGLCoco/MyOpenGLCoco/blog/blog_cxx_01.cpp
@@ -0,0 +1,85 @@
+//
+//  Chapter3_8_Primitives.cpp
+//  MyOpenGLCoco
+//
+//  Created by lichong on 2017/7/30.
+//  Copyright © 2017年 lichong. All rights reserved.
+//
+
+// Main entry point for GLUT based programs
+
+/**
+ 实例程序：三角形
+ http://blog.csdn.net/u010223072/article/details/44309113
+ */
+
+#include <GLTools.h>
+#include "GLShaderManager.h"
+
+#include <math.h>
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+/////////////////////////////////////////////////////////////////////////////////
+// An assortment of needed classes
+GLBatch triangleBatch;
+GLShaderManager		shaderManager;
+
+void ChangeSize(int w,int h)
+{
+    glViewport(0, 0, w, h);
+}
+void SetupRC()
+{
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    shaderManager.InitializeStockShaders();
+    GLfloat vVerts[] = {
+        -0.5f,0.0f,0.0f,
+        0.5f,0.0f,0.0f,
+        0.0f,0.5f,0.0f,
+    };
+    triangleBatch.Begin(GL_TRIANGLES,3);
+    triangleBatch.CopyVertexData3f(vVerts);
+    triangleBatch.End();
+}
+void RenderScene(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    GLfloat vRed[] = {1.0f,0.0f,0.0f,1.0f};
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY,vRed);
+    triangleBatch.Draw();
+    glutSwapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+    //设置当前工作目录，针对MAC OS X
+    gltSetWorkingDirectory(argv[0]);
+    //初始化GLUT库
+    glutInit(&argc, argv);
+    /*初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指
+     双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区*/
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+    //GLUT窗口大小，标题窗口
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Triangle");
+    //注册回调函数 窗口改变
+    glutReshapeFunc(ChangeSize);
+    //注册回调函数 渲染
+    glutDisplayFunc(RenderScene);
+    //渲染环境检测
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+//    
+//    
+    SetupRC();
+//消息循环
+    glutMainLoop();
+    return 0;
+}
