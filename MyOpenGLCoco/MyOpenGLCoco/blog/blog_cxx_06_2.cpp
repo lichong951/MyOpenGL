fe078306c0c71dbe8366b017adb5a0da8e150a62
lichong <lichong@7moor.com>
2017-08-06 17:59:17 +0800

    【blog实例】06_3模拟行星系统;06_4:模拟机器人手臂

---
+++ MyOpenGLCoco/MyOpenGLCoco/blog/blog_cxx_06_2.cpp
@@ -0,0 +1,120 @@
+//
+//  blog_cxx_06_2.cpp
+//  MyOpenGLCoco
+//
+//  Created by lichong on 2017/8/6.
+//  Copyright © 2017年 lichong. All rights reserved.
+//
+
+/**
+ 绘制一个裁剪掉了3/4的线框圆(除了视景体的6个裁剪平面外，还可以另外再指定最多可达6个的其他裁剪平面，对视景体施加进一步的限制)
+ http://blog.csdn.net/u010223072/article/details/44620871
+ */
+#include <stdio.h>
+#include <glut/glut.h>
+
+void init(void){
+    //背景颜色
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    /*设置着色模式：GL_FLAT：恒定着色，用某个顶点颜色来渲染整个图元
+     GL_SMOOTH：光滑着色，独立的处理图元中各个顶点的颜色。*/
+    glShadeModel(GL_FLAT);
+    
+}
+
+void display(void){
+    //其它裁剪面方程式Ax + By + Cz + D = 0系数
+    GLdouble eqn[4] = {0.0, 1.0, 0.0, 0.0};
+    GLdouble eqn2[4] = {1.0, 0.0, 0.0, 0.0};
+    //清除颜色缓冲区
+    glClear(GL_COLOR_BUFFER_BIT);
+    //设置颜色
+    glColor3f(1.0, 1.0, 1.0);
+    /*矩阵堆栈，可以消除上一次的变换对本次变换的影响，一系列像平移，旋转，
+     缩放等的矩阵变换操作放在它们之间可以简化操作，不然每次改变都要重新
+     设置矩阵模式*/
+    glPushMatrix();
+    //Z轴负方向平移5个单位
+    glTranslatef(0.0, 0.0, -5.0);
+    
+    /* 裁剪掉y < 0 的部分 */
+    glClipPlane(GL_CLIP_PLANE0, eqn);
+    //激活
+    glEnable(GL_CLIP_PLANE0);
+    /* 裁剪掉x < 0 的部分 */
+    glClipPlane(GL_CLIP_PLANE1, eqn2);
+    //激活
+    glEnable(GL_CLIP_PLANE1);
+    /*逆时针方向绕着从原点到(1.0,0.0,0.0)的直线旋转90°*/
+    glRotatef(90.0, 1.0, 0.0, 0.0);
+    //绘制圆
+    glutWireSphere(1.0, 20, 16);
+    glPopMatrix();
+    //让缓冲区中的图像最终显示，而不是一直放在缓冲区等待
+    glFlush();
+}
+
+void reshape(int w,int h){
+    //设置视口，即显示区域
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    //指定当前矩阵（一般与glLoadIdentity一起使用）
+    glMatrixMode(GL_PROJECTION);
+    //重置当前指定的矩阵为单位矩阵,类似于一个复位操作
+    glLoadIdentity();
+    /*透视投影,参数：角度，宽高比，近平面，远平面*/
+    gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+    
+    //接下来的矩阵操作设置为模型操作,即平移，旋转，缩放等操作
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key,int x,int y){
+    switch (key) {
+        case 27:
+//            exit(0);
+            break;
+            
+        default:
+            break;
+    }
+}
+
+int main(int argc,char** argv){
+    //初始化GLUT库
+    glutInit(&argc, argv);
+    /*初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指
+     双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区*/
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    //GLUT窗口大小，标题窗口
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow (argv[0]);
+    
+    init();
+    //注册回调函数 渲染
+    glutDisplayFunc(display);
+    //注册回调函数 窗口改变
+    glutReshapeFunc(reshape);
+    //键盘按键回调
+    glutKeyboardFunc(keyboard);
+    //循环
+    glutMainLoop();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MyOpenGLCoco/MyOpenGLCoco/blog/blog_cxx_06_3.cpp
@@ -0,0 +1,118 @@
+//
+//  blog_cxx_06_3.cpp
+//  MyOpenGLCoco
+//
+//  Created by lichong on 2017/8/6.
+//  Copyright © 2017年 lichong. All rights reserved.
+//
+/**
+ 模拟行星系统
+ http://blog.csdn.net/u010223072/article/details/44620871
+ */
+#include <stdio.h>
+#include "GLTools.h"
+#include "GLShaderManager.h"
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#endif
+
+static int day=200;
+void display()
+{
+    //清除颜色缓冲区 深度缓冲区
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    //开启深度测试
+    glEnable(GL_DEPTH_TEST);
+    //指定当前矩阵（一般与glLoadIdentity一起使用）
+    glMatrixMode(GL_PROJECTION);
+    //重置当前指定的矩阵为单位矩阵,类似于一个复位操作
+    glLoadIdentity();
+    /*透视投影,参数：角度，宽高比，近平面，远平面*/
+    gluPerspective(75, 1, 1, 400);
+    //接下来的矩阵操作设置为模型操作,即平移，旋转，缩放等操作
+    glMatrixMode(GL_MODELVIEW);
+    //重置当前指定的矩阵为单位矩阵,类似于一个复位操作
+    glLoadIdentity();
+    //视点转换 参考http://blog.csdn.net/augusdi/article/details/20470813
+    gluLookAt(0, -200, 200, 0, 0, 0, 0, 0, 1);
+    
+    //太阳
+    //设置颜色
+    glColor3f(1.0f, 0.0f, 0.0f);
+    //该函数用于渲染一个球体。球体球心位于原点，参数分别为
+    glutSolidSphere(69.6, 50, 50);
+    //地球
+    glColor3f(0.0f, 0.0f, 1.0f);
+    /*正确地顺序是要先平移再旋转，代码的写法要相反*/
+    //旋转
+    glRotatef(day , 0.0f, 0.0f, -1.0f);
+     //平移
+    glTranslatef(150, 0.0f, 0.0f);
+    //该函数用于渲染一个球体。球体球心位于原点，参数分别为
+    glutSolidSphere(15.945, 50, 50);
+    //月亮
+    //设置颜色
+    glColor3f(1.0f, 1.0f, 0.0f);
+    //旋转
+    glRotatef(day / 30.0*360.0 - day , 0.0f, 0.0f, -1.0f);
+    //平移
+    glTranslatef(38, 0.0f, 0.0f);
+    //该函数用于渲染一个球体。球体球心位于原点，参数分别为
+    glutSolidSphere(4.345, 50, 50);
+    //后台缓冲区交换到前台缓冲区显示
+    glutSwapBuffers();
+    
+}
+
+void play()
+{
+    day++;
+    if (day>=360) {
+        day=0;
+    }
+    display();
+    //重新绘制窗口
+    glutPostRedisplay();
+}
+
+int main(int argc,char** argv){
+    //初始化GLUT库
+    glutInit(&argc, argv);
+    /*初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指
+     双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区*/
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+    //GLUT窗口大小，标题窗口
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow (argv[0]);
+    //注册回调函数 渲染
+     glutDisplayFunc(display);
+    //消息循环处于空闲的时候响应 http://blog.sina.com.cn/s/blog_680374c30100l7lh.html
+    glutIdleFunc(play);
+    glutMainLoop();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
